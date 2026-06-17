f6042bc8f91d272e71908326a35bf871e5c95097
Midhun <midhungretta@gmail.com>
2026-06-17 10:47:41 +0530

    debug change to false

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def home():
     return render_template("index.html", answer=answer)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
